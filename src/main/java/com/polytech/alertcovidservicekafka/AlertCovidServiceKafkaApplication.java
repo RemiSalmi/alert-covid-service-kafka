1056852ae290895ec312b52676eeb1615e3f9982
Remi Salmi <remi.salmi@gmail.com>
2020-10-24 17:27:31 +0200

    init project

---
+++ src/main/java/com/polytech/alertcovidservicekafka/AlertCovidServiceKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.alertcovidservicekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertCovidServiceKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertCovidServiceKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polytech/alertcovidservicekafka/AlertCovidServiceKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.alertcovidservicekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertCovidServiceKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
