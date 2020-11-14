20994b251baca0a885648c1e79d2b6510827cc98
Simon <gayetsimon@gmail.com>
2020-11-14 22:47:33 +0100

    add auth

--- src/main/java/com/polytech/alertcovidservicekafka/controllers/LocationController.java
+++ src/main/java/com/polytech/alertcovidservicekafka/controllers/LocationController.java
@@ -1,23 +1,28 @@
 package com.polytech.alertcovidservicekafka.controllers;
 
-
-
-import com.grum.geocalc.Coordinate;
-import com.grum.geocalc.EarthCalc;
-import com.grum.geocalc.Point;
-
-import com.polytech.alertcovidservicekafka.models.Location;
-import com.polytech.alertcovidservicekafka.models.KafkaProducer;
-import com.polytech.alertcovidservicekafka.models.LocationsStreamSingleton;
-import com.polytech.alertcovidservicekafka.models.PositiveCase;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.polytech.alertcovidservicekafka.models.*;
+import com.polytech.alertcovidservicekafka.services.JsonBodyHandler;
 import com.polytech.alertcovidservicekafka.services.LocationLogic;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.sql.Time;
-import java.sql.Timestamp;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Supplier;
 
 
 @RestController
@@ -29,24 +34,94 @@ public class LocationController {
 
     private LocationsStreamSingleton locationsStream = LocationsStreamSingleton.getInstance();
 
-    @GetMapping(value = "/test")
+    @GetMapping
     public LinkedList<Location> getLocation() {
         return locationsStream.getLocations();
     }
 
     @PostMapping
-    public String create(@RequestBody Location location) {
-        System.out.println("Start");
-        System.out.println(location);
-        kafkaProducer.sendMessage(location, "yxffg513-covid_alert");
-        System.out.println("Fin");
-        return "Publish";
+    public String create(@RequestHeader("Authorization") String authorization,@RequestBody Location location) {
+        Long id_user = this.getIdFromAuthorization(authorization);
+        if (id_user.equals(location.getId_user())){
+            kafkaProducer.sendMessage(location, "yxffg513-covid_alert");
+            return "Publish";
+        } else {
+            throw new ResponseStatusException( HttpStatus.UNAUTHORIZED, "Your not authorized to do this action" ) ;
+        }
     }
 
     @PostMapping(value="/positive")
-    public List<Location> getContactCase(@RequestBody PositiveCase positiveCase) {
-        LocationLogic locationLogic = new LocationLogic();
-        List<Location> contactLocation = locationLogic.getContactLocation(locationsStream.getLocations(), positiveCase.getId_user(), positiveCase.getDate());
-        return contactLocation;
+    public List<Location> getContactCase(@RequestHeader("Authorization") String authorization, @RequestBody PositiveCase positiveCase) {
+        System.out.println(positiveCase);
+        Long id_user = this.getIdFromAuthorization(authorization);
+        if (id_user.equals(positiveCase.getId_user())){
+            LocationLogic locationLogic = new LocationLogic();
+            List<Location> contactLocation = locationLogic.getContactLocation(locationsStream.getLocations(), positiveCase.getId_user(), positiveCase.getDate());
+            //Call insert location (location by location)
+            //Call notification list ID
+            return contactLocation;
+        } else {
+            throw new ResponseStatusException( HttpStatus.UNAUTHORIZED, "Your not authorized to do this action" ) ;
+        }
+    }
+
+
+    ////////////////////////////// PRIVATE METHOD  ////////////////////////////////////////////////
+
+    private void postLocationService_thenCorrect(String contactLocationJson, String authorization)
+            throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://localhost:8091/locations/");
+        System.out.println();
+
+        StringEntity entity = new StringEntity(contactLocationJson);
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
+        httpPost.setHeader("Authorization", authorization);
+
+        CloseableHttpResponse response = client.execute(httpPost);
+        assert(response.getStatusLine().getStatusCode() == 200);
+        client.close();
+    }
+
+    private void postNotificationService_thenCorrect(String contactLocationJson, String authorization)
+            throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://localhost:8089/notification/");
+        System.out.println();
+
+        StringEntity entity = new StringEntity(contactLocationJson);
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
+        httpPost.setHeader("Authorization", authorization);
+
+        CloseableHttpResponse response = client.execute(httpPost);
+        assert(response.getStatusLine().getStatusCode() == 200);
+        client.close();
+    }
+
+    private long getIdFromAuthorization(String authorization){
+        String token = authorization.split(" ")[1];
+        DecodedJWT jwt = JWT.decode(token);
+        String email = jwt.getClaim("email").asString();
+
+        var client = HttpClient.newHttpClient();
+        String url = "http://146.59.234.45:8081/users/" + email;
+        var request = HttpRequest.newBuilder(
+                URI.create(url))
+                .header("Authorization", authorization)
+                .build();
+
+        HttpResponse<Supplier<User>> response = null;
+        try {
+            response = client.send(request, new JsonBodyHandler<>(User.class));
+            return response.body().get().id_user;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            throw new ResponseStatusException( HttpStatus.INTERNAL_SERVER_ERROR, "can't get your id from your token" ) ;
+        }
+
     }
 }
